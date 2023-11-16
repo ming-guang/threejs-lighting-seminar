22b194de28045df2e3847640fb9b9e6892135676
ming-guang <athoni-contact@relay.akimitsu.dev>
2023-11-17 03:31:05 +0700

    feat: implement ambient light

--- src/components/ScenceLight.vue
+++ src/components/ScenceLight.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { storeToRefs } from "pinia";
 import { useSettingsStore } from "../stores/settings";
-const { lightType, directionalLightSettings } = storeToRefs(useSettingsStore());
+const { lightType, directionalLightSettings, ambientLightSettings } = storeToRefs(useSettingsStore());
 </script>
 
 <template>
     <TresDirectionalLight v-if="lightType === 'directional-light'" :position="directionalLightSettings.position"
         :intensity="directionalLightSettings.intensity" :color="directionalLightSettings.color" :cast-shadow="true" />
+    <TresAmbientLight v-if="lightType === 'ambient-light'" :intensity="ambientLightSettings.intensity"
+        :color="ambientLightSettings.color" :cast-shadow="true" />
 </template>
