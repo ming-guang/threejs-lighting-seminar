205a12c947640c34df3a43516e0fdd278358f26c
ming-guang <athoni-contact@relay.akimitsu.dev>
2023-11-17 03:26:35 +0700

    feat: implement ambient light control panel

---
+++ src/components/AmbientLightControl.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import * as THREE from "three";
+import { storeToRefs } from "pinia";
+import { useSettingsStore } from "../stores/settings";
+const settings = useSettingsStore();
+const { ambientLightSettings } = storeToRefs(settings);
+import { ColorPicker } from "vue-accessible-color-picker";
+let color: string = ambientLightSettings.value.color;
+let intensity: number = ambientLightSettings.value.intensity;
+
+const onChange = () => {
+    settings.setAmbientLight({
+        color,
+        intensity,
+    });
+};
+
+const colorChange = (e) => {
+    color = e.colors.hex.substring(0, 7);
+    onChange();
+};
+
+</script>
+
+<template>
+    <div class="mt-2">
+        <label class="block mb-2 text-sm font-medium text-gray-900">Color</label>
+        <ColorPicker alpha-channel="hide" @color-change="colorChange" />
+    </div>
+
+    <div class="mt-2">
+        <label for="intensity" class="block mb-2 text-sm font-medium text-gray-900">Intensity</label>
+        <input id="intensity" v-model="intensity" type="range" min="0" max="1" step="0.01"
+            class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer" @change="onChange()">
+    </div>
+</template>
