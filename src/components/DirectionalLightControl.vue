dfb595d36e6e5c6b6e6d91afc985ecaabc58957f
ming-guang <athoni-contact@relay.akimitsu.dev>
2023-11-17 03:32:52 +0700

    chore: run formatter

--- src/components/DirectionalLightControl.vue
+++ src/components/DirectionalLightControl.vue
@@ -10,52 +10,78 @@ let intensity: number = directionalLightSettings.value.intensity;
 let { x, y, z } = directionalLightSettings.value.position;
 
 const onChange = () => {
-    console.log({x, y, z});
-    settings.setDirectionalLight({
-        color,
-        intensity,
-        position: new THREE.Vector3(x, y, z),
-    });
+  console.log({ x, y, z });
+  settings.setDirectionalLight({
+    color,
+    intensity,
+    position: new THREE.Vector3(x, y, z),
+  });
 };
 
 const colorChange = (e) => {
-    color = e.colors.hex.substring(0, 7);
-    onChange();
+  color = e.colors.hex.substring(0, 7);
+  onChange();
 };
-
 </script>
 
 <template>
-    <div class="mt-2">
-        <label class="block mb-2 text-sm font-medium text-gray-900">Color</label>
-        <ColorPicker alpha-channel="hide" @color-change="colorChange" />
-    </div>
+  <div class="mt-2">
+    <label class="block mb-2 text-sm font-medium text-gray-900">Color</label>
+    <ColorPicker alpha-channel="hide" @color-change="colorChange" />
+  </div>
 
-    <div class="mt-2">
-        <label for="intensity" class="block mb-2 text-sm font-medium text-gray-900">Intensity</label>
-        <input id="intensity" v-model="intensity" type="range" min="0" max="1" step="0.01"
-            class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer"
-            @change="onChange()" >
-    </div>
+  <div class="mt-2">
+    <label for="intensity" class="block mb-2 text-sm font-medium text-gray-900"
+      >Intensity</label
+    >
+    <input
+      id="intensity"
+      v-model="intensity"
+      type="range"
+      min="0"
+      max="1"
+      step="0.01"
+      class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer"
+      @change="onChange()"
+    />
+  </div>
 
-    <div class="grid grid-cols-3 grid-gap-4">
-        <div class="mt-2">
-            <label for="x" class="block mb-2 text-sm font-medium text-gray-900">x</label>
-            <input type="number" id="x"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                v-model="x" @change="onChange()">
-        </div>
-        <div class="mt-2">
-            <label for="y" class="block mb-2 text-sm font-medium text-gray-900">y</label>
-            <input type="number" id="y"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                v-model="y" @change="onChange()">
-        </div>
-        <div class="mt-2">
-            <label for="z" class="block mb-2 text-sm font-medium text-gray-900">z</label>
-            <input type="number" id="z"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                v-model="z" @change="onChange()">
-        </div>
+  <div class="grid grid-cols-3 grid-gap-4">
+    <div class="mt-2">
+      <label for="x" class="block mb-2 text-sm font-medium text-gray-900"
+        >x</label
+      >
+      <input
+        type="number"
+        id="x"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        v-model="x"
+        @change="onChange()"
+      />
     </div>
+    <div class="mt-2">
+      <label for="y" class="block mb-2 text-sm font-medium text-gray-900"
+        >y</label
+      >
+      <input
+        type="number"
+        id="y"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        v-model="y"
+        @change="onChange()"
+      />
+    </div>
+    <div class="mt-2">
+      <label for="z" class="block mb-2 text-sm font-medium text-gray-900"
+        >z</label
+      >
+      <input
+        type="number"
+        id="z"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        v-model="z"
+        @change="onChange()"
+      />
+    </div>
+  </div>
 </template>
--- src/components/ScenceLight.vue
+++ src/components/ScenceLight.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
 import { storeToRefs } from "pinia";
 import { useSettingsStore } from "../stores/settings";
-const { lightType, directionalLightSettings, ambientLightSettings } = storeToRefs(useSettingsStore());
+const { lightType, directionalLightSettings, ambientLightSettings } =
+  storeToRefs(useSettingsStore());
 </script>
 
 <template>
-    <TresDirectionalLight v-if="lightType === 'directional-light'" :position="directionalLightSettings.position"
-        :intensity="directionalLightSettings.intensity" :color="directionalLightSettings.color" :cast-shadow="true" />
-    <TresAmbientLight v-if="lightType === 'ambient-light'" :intensity="ambientLightSettings.intensity"
-        :color="ambientLightSettings.color" :cast-shadow="true" />
+  <TresDirectionalLight
+    v-if="lightType === 'directional-light'"
+    :position="directionalLightSettings.position"
+    :intensity="directionalLightSettings.intensity"
+    :color="directionalLightSettings.color"
+    :cast-shadow="true"
+  />
+  <TresAmbientLight
+    v-if="lightType === 'ambient-light'"
+    :intensity="ambientLightSettings.intensity"
+    :color="ambientLightSettings.color"
+    :cast-shadow="true"
+  />
 </template>
