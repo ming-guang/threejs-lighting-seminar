35e26a6d8bd11d4e6c7366d666b939cb53e0d05a
ming-guang <athoni-contact@relay.akimitsu.dev>
2023-11-17 02:46:21 +0700

    feat: implement directional light control panel

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -1,35 +1,50 @@
 <script setup lang="ts">
 import { storeToRefs } from "pinia";
-import { useSettingsStore } from "../stores/settings";
+import { type SupportedLightType, useSettingsStore } from "../stores/settings";
 import { models } from "../stores/models";
+import DirectionalLightControl from "./DirectionalLightControl.vue";
 const settings = useSettingsStore();
 const settingsRef = storeToRefs(settings);
 
+let lightType: SupportedLightType = settings.lightType;
 let modelKey = settings.modelKey;
 const onUpdateModel = () => {
-  settings.updateModel(modelKey);
+    settings.updateModel(modelKey);
+};
+const onUpdateLightType = () => {
+    if (lightType === "none") {
+        settings.setNoLight();
+        return;
+    }
+
+    if (lightType === "directional-light") {
+        settings.setDirectionalLight({});
+        return;
+    }
 };
 </script>
 
 <template>
-  <div class="m-2.5">
-    <label for="model" class="block mb-2 text-sm font-medium text-gray-900"
-      >Model</label
-    >
-    <select
-      id="model"
-      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-      v-model="modelKey"
-      @change="onUpdateModel()"
-    >
-      <option
-        v-for="(model, key) in models"
-        :key="key"
-        :value="key"
-        :select="key === modelKey"
-      >
-        {{ model.name }}
-      </option>
-    </select>
-  </div>
+    <div class="m-2.5">
+        <label for="model" class="block mb-2 text-sm font-medium text-gray-900">Model</label>
+        <select id="model"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            v-model="modelKey" @change="onUpdateModel()">
+            <option v-for="(model, key) in models" :key="key" :value="key" :select="key === modelKey">
+                {{ model.name }}
+            </option>
+        </select>
+
+        <label for="light" class="block mt-2 mb-2 text-sm font-medium text-gray-900">Light</label>
+        <select id="light"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            v-model="lightType" @change="onUpdateLightType()">
+            <option :value="'none'" :select="lightType === 'none'">None</option>
+            <option :value="'directional-light'" :select="lightType === 'directional-light'">Directional light</option>
+        </select>
+
+        <div v-if="lightType === 'directional-light'">
+            <DirectionalLightControl />
+        </div>
+    </div>
 </template>
