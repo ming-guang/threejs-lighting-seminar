923df3e010af60db812ec49aad56b4dda737b25e
ming-guang <athoni-contact@relay.akimitsu.dev>
2023-11-24 15:15:53 +0700

    fix: reimplement missing shadow

--- src/components/ScenceLight.vue
+++ src/components/ScenceLight.vue
@@ -3,73 +3,46 @@ import { Color } from "three";
 import { storeToRefs } from "pinia";
 import { useSettingsStore } from "../stores/settings";
 const {
-  lightType,
-  directionalLightSettings,
-  ambientLightSettings,
-  hemisphereLightSettings,
-  pointLightSettings,
-  spotLightSettings,
-  fogSettings,
+    lightType,
+    directionalLightSettings,
+    ambientLightSettings,
+    hemisphereLightSettings,
+    pointLightSettings,
+    spotLightSettings,
+    fogSettings,
 } = storeToRefs(useSettingsStore());
 function stringToColor(color: string): Color {
-  return new Color(color);
+    return new Color(color);
 }
 </script>
 
 <template>
-  <TresDirectionalLight
-    v-if="lightType === 'directional-light'"
-    :position="directionalLightSettings.position"
-    :intensity="directionalLightSettings.intensity"
-    :color="stringToColor(directionalLightSettings.color)"
-    :cast-shadow="true"
-  />
-  <TresGroup v-else></TresGroup>
+    <TresDirectionalLight v-if="lightType === 'directional-light'" :position="directionalLightSettings.position"
+        :intensity="directionalLightSettings.intensity" :color="stringToColor(directionalLightSettings.color)"
+        :cast-shadow="true" />
+    <TresGroup v-else></TresGroup>
 
-  <TresAmbientLight
-    v-if="lightType === 'ambient-light'"
-    :intensity="ambientLightSettings.intensity"
-    :color="stringToColor(ambientLightSettings.color)"
-    :cast-shadow="true"
-  />
-  <TresGroup v-else></TresGroup>
+    <TresAmbientLight v-if="lightType === 'ambient-light'" :intensity="ambientLightSettings.intensity"
+        :color="stringToColor(ambientLightSettings.color)" :cast-shadow="true" />
+    <TresGroup v-else></TresGroup>
 
-  <TresHemisphereLight
-    v-if="lightType === 'hemisphere-light'"
-    :color="stringToColor(hemisphereLightSettings.skyColor)"
-    :ground-color="stringToColor(hemisphereLightSettings.groundColor)"
-    :intensity="hemisphereLightSettings.intensity"
-    :cast-shadow="true"
-  />
-  <TresGroup v-else></TresGroup>
+    <TresHemisphereLight v-if="lightType === 'hemisphere-light'" :color="stringToColor(hemisphereLightSettings.skyColor)"
+        :ground-color="stringToColor(hemisphereLightSettings.groundColor)" :intensity="hemisphereLightSettings.intensity"
+        :cast-shadow="true" />
+    <TresGroup v-else></TresGroup>
 
-  <TresPointLight
-    v-if="lightType === 'point-light'"
-    :color="stringToColor(pointLightSettings.color)"
-    :intensity="pointLightSettings.intensity"
-    :distance="pointLightSettings.distance"
-    :decay="pointLightSettings.decay"
-    :position="pointLightSettings.position"
-  />
-  <TresGroup v-else></TresGroup>
+    <TresPointLight v-if="lightType === 'point-light'" :color="stringToColor(pointLightSettings.color)"
+        :intensity="pointLightSettings.intensity" :distance="pointLightSettings.distance" :decay="pointLightSettings.decay"
+        :position="pointLightSettings.position" :cast-shadow="true" />
+    <TresGroup v-else></TresGroup>
 
-  <TresSpotLight
-    v-if="lightType === 'spot-light'"
-    :color="stringToColor(spotLightSettings.color)"
-    :intensity="spotLightSettings.intensity"
-    :distance="spotLightSettings.distance"
-    :angle="spotLightSettings.angle"
-    :penumbra="spotLightSettings.penumbra"
-    :decay="spotLightSettings.decay"
-    :position="spotLightSettings.position"
-  />
-  <TresGroup v-else></TresGroup>
+    <TresSpotLight v-if="lightType === 'spot-light'" :color="stringToColor(spotLightSettings.color)"
+        :intensity="spotLightSettings.intensity" :distance="spotLightSettings.distance" :angle="spotLightSettings.angle"
+        :penumbra="spotLightSettings.penumbra" :decay="spotLightSettings.decay" :position="spotLightSettings.position"
+        :cast-shadow="true" />
+    <TresGroup v-else></TresGroup>
 
-  <TresFog
-    v-if="fogSettings.enabled"
-    :color="stringToColor(fogSettings.color)"
-    :near="fogSettings.near"
-    :far="fogSettings.far"
-  />
-  <TresGroup v-else></TresGroup>
+    <TresFog v-if="fogSettings.enabled" :color="stringToColor(fogSettings.color)" :near="fogSettings.near"
+        :far="fogSettings.far" />
+    <TresGroup v-else></TresGroup>
 </template>
